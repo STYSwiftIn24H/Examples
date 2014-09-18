bf7ca7dd4fbeaeb4ceafc6e7131cef4f8a871622
STYSwiftIn24H <styswiftin24h@gmail.com>
2014-09-17 20:18:09 -0400

    Update Listing 7.6 and 7.8 examples.
    Listing 7.6 was revised to add iPhone 6 and iPhone 6 Plus.
    Listing 7.8 was revised to make range 1...6 from 1...7.

--- Ch07/Ch07 Loops.playground/section-1.swift
+++ Ch07/Ch07 Loops.playground/section-1.swift
@@ -93,7 +93,9 @@ let retinaiPhonesAndYears = ["iPhone 4" : 2010,
                             "iPhone 4S" : 2011,
                              "iPhone 5" : 2012,
                             "iPhone 5C" : 2013,
-                            "iPhone 5S" : 2013]
+                            "iPhone 5S" : 2013
+                             "iPhone 6" : 2014,
+                        "iPhone 6 Plus" : 2014]
 for (iPhone, year) in retinaiPhonesAndYears {
     println("The \(iPhone) was released in \(year)")
 }
@@ -117,7 +119,7 @@ var results = [Int]()
 for count in 1...100 {
     let remainder = count % 7
     switch remainder {
-    case 1...7:
+    case 1...6:
         continue
     default:
         results.append(count)
